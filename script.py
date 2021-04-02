2d46dc27f3f535c07a67ef42868c610d10589995
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-04-02 18:10:49 +0100

    AVANÇOS

--- script.py
+++ script.py
@@ -5,4 +5,22 @@ c=clfilter(opt="do:")
 
 for page in c.slurp():
     nome = findall(r'<title>([\w \-]+)</title>', page)
-    print(nome[1])
+    print(nome)
+    #familiares = findall(r'<a\s+href=(.*?id=(\d+)"?)>(.*?)</a>', page)
+    dataNascimento = findall(r'<div align="center">\s+(\*\s+\w+|\s+\+\s+\w+).*?<nobr>(\d+\.\d+\.\d+)</nobr>',page)
+    filhos = findall(r'<LI><A HREF=pessoas.php\?id=(\d+)>\s+([\w \-]+)</A>',page)
+    print("A DATA DE NASCIMENTO/MORTE É (* indica nascimento, + indica morte):")
+    for data in dataNascimento:
+        print(data)
+    print("OS FILHOS DE "+nome[0]+" SÃO:")
+    for id,nome in filhos:
+        print(f"==> {id} {nome.strip()}")
+    pai=findall(r'<B>Pai:</B>\s*?<A HREF=pessoas.php\?id=(\d+)>\s*?([\w \-]+)</A>',page)
+    print("O PAI É:")
+    for id,nome in pai:
+        print(f"==> {id} {nome.strip()}")
+    mae=findall(r'<B>Mãe:</B>\s*?<A HREF=pessoas.php\?id=(\d+)>\s*?([\w \-]+)</A>',page)
+    print("A MAE É:")
+    for id,nome in mae:
+        print(f"==> {id} {nome.strip()}")
+    
